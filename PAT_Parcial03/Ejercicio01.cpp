39ce6331330b005f966f9a919edf7eaa07178bd7
Ricardo <jrguerram457@gmail.com>
2023-11-26 11:43:49 -0600

    ejercicio01

--- PAT_Parcial03/Ejercicio01.cpp
+++ PAT_Parcial03/Ejercicio01.cpp
@@ -1,6 +1,53 @@
 #include "Ejercicio01.h"
+#include <vector>
+#include <algorithm>
 
 Node<int>* Ejercicio01::mergeLists(Node<Node<int>*>* lists)
 {
-    return nullptr;
+
+    auto compare = [](Node<int>* a, Node<int>* b) {
+        return a->value > b->value;
+        };
+
+
+    std::vector<Node<int>*> nodes;
+
+    while (lists != nullptr)
+    {
+        Node<int>* currentList = lists->value;
+
+        while (currentList != nullptr)
+        {
+            nodes.push_back(currentList);
+            currentList = currentList->next;
+        }
+
+        lists = lists->next;
+    }
+
+
+    std::sort(nodes.begin(), nodes.end(), compare);
+
+    Node<int>* result = nullptr;
+    Node<int>* tail = nullptr;
+    for (Node<int>* node : nodes)
+    {
+        if (result == nullptr)
+        {
+            result = node;
+            tail = node;
+        }
+        else
+        {
+            tail->next = node;
+            tail = node;
+        }
+    }
+
+    if (tail != nullptr)
+    {
+        tail->next = nullptr;
+    }
+
+    return result;
 }
